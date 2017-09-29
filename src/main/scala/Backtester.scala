e6cc155d130f088d97b3410a2075662a082aaf8f
Paul Batterman <dev.Batterman.Paul@dev.bwater.com>
2017-09-29 12:00:03 -0400

    update

--- src/main/scala/Backtester.scala
+++ src/main/scala/Backtester.scala
@@ -1,5 +1,5 @@
 import scala.io._
-import breeze.linalg._
+import breeze.linalg.DenseVector
 import breeze.numerics._
 import breeze.plot._
 
@@ -36,13 +36,38 @@ object Backtester {
     fig.refresh()
   }
 
+  def getMoney(state: State, date: Int): Double = {
+    val stockData = state.stockData
+    state.holdings.map((secnum) => {
+      val (sec, num): (String, Int) = secnum
+      val price: Double = stockData.get(sec).map((timepricedata) => timepricedata(date)._2).getOrElse(-1) // should never be -1
+      num * price
+    }).sum + state.cash
+  }
+
   def main(args: Array[String]): Unit = {
     val stocks = Seq("sap", "ibm")
-    val stockData: Seq[Seq[(Double, Double)]] = stocks.map(getDatePriceData(_))
-    val minLength = stockData.map((data) => data.length).min
-    val trimmedData = stockData.map((data) => data.take(minLength))
-    val rightData: StockData = stocks.zip(trimmedData.map(_.reverse)).toMap
-    plotStockPrices(rightData)
+    val rawData: Seq[Seq[(Double, Double)]] = stocks.map(getDatePriceData(_))
+    val minLength = rawData.map((data) => data.length).min
+    val trimmedData = rawData.map((data) => data.take(minLength))
+    val stockData: StockData = stocks.zip(trimmedData.map(_.reverse)).toMap
+
+//    Rule(security:String, sellThreshold:Double, buyThreshold:Double, buyCountDailyMax:Int, sellCountDailyMax:Int)
+
+    val startDate = 0
+    val endDate = minLength
+    val initCash = 1000
+    val rules = Map("sap" -> Rule("sap", 0, 0, 1000000, 0))
+    val init = State(stockData, Map[String, Int](), initCash, startDate)
+    val myhistory: Seq[State] = RuleInterpreter.runRules(init, stockData, rules, startDate, endDate)
+
+    println(myhistory.take(10).map(state => (state.cash, state.holdings)))
+
+    val mymoney: Seq[Double] = myhistory.zip(startDate until endDate).map(statedate => getMoney(statedate._1, statedate._2))
+    val days = (startDate until endDate).map (_.toDouble)
+    val timemoneydata: Seq[(Double, Double)] = days.zip(mymoney)
+
+    plotStockPrices(stockData.updated("mydata", timemoneydata))
 
   }
 
@@ -53,21 +78,70 @@ object Backtester {
                           date: Int)
 
   object RuleInterpreter {
-    def eval(state: State, rules: Map[String,Rule]): State = {
-//      state.holdings.data.map((sec, amount) => {})
-      def datePrice(security: String, date: Int) = state.stockData.get(security).map((seq) => seq(date)._2)
-      for {
-        (security, num) <- state.holdings
-        todayPrice = datePrice(security, state.date)
-        priceDelta = todayPrice - datePrice(security, state.date - 1)
-        numBought = if (rules(security).buyThreshold > priceDelta) rules(security).buyCountDailyMax.min(state.cash / todayPrice) else 0
-        updatedState = state.copy(cash = (state.cash - numBought * todayPrice), holdings = state.holdings.updated(security, numBought + num))
-        numSold = if (rules(security).sellThreshold < priceDelta) rules(security).sellCountDailyMax.min(num) else 0
-        updatedState = state.copy(cash = (state.cash + numSold * todayPrice), holdings = state.holdings.updated(security, num - numSold))
+//    def eval(state: State, rules: Map[String,Rule]): State = {
+//      //      state.holdings.data.map((sec, amount) => {})
+//      def datePrice(security: String, date: Int) = state.stockData.get(security).map((seq) => seq(date)._2)
+//
+//      for {
+//        (security, num) <- state.holdings
+//        todayPrice = datePrice(security, state.date)
+//        priceDelta = todayPrice - datePrice(security, state.date - 1)
+//        numBought = if (rules(security).buyThreshold > priceDelta) rules(security).buyCountDailyMax.min(state.cash / todayPrice) else 0
+//        updatedState = state.copy(cash = (state.cash - numBought * todayPrice), holdings = state.holdings.updated(security, numBought + num))
+//        numSold = if (rules(security).sellThreshold < priceDelta) rules(security).sellCountDailyMax.min(num) else 0
+//        updatedState = state.copy(cash = (state.cash + numSold * todayPrice), holdings = state.holdings.updated(security, num - numSold))
+//      }
+//    }
+
+    def eval(initstate: State, rules: Map[String,Rule]): State = {
+      //      state.holdings.data.map((sec, amount) => {})
+      def datePrice(state: State, security: String, date: Int): Double = state.stockData.get(security).map((seq) => seq(date)._2).getOrElse(0)
+      println("prefold")
+      initstate.stockData.foldLeft(initstate)((state, secdata) => {
+        val (security, _) = secdata
+        val num: Int = state.holdings.getOrElse(security, 0)
+        val todayPrice: Double = datePrice(state, security, state.date)
+        val priceDelta: Double = todayPrice - datePrice(state, security, state.date - 1)
+        val numBought = rules.get(security).map(r =>
+          if (r.buyThreshold > priceDelta)
+            r.buyCountDailyMax.min( (state.cash / todayPrice).floor.toInt )
+          else 0).getOrElse(0)
+        val updatedBought = state.copy(holdings = state.holdings.
+          updated(security, num + numBought), cash = state.cash - numBought * todayPrice)
+        println(numBought)
+        val numSold = rules.get(security).map(r =>
+          if (r.sellThreshold < priceDelta)
+            r.sellCountDailyMax.min(num)
+          else 0).getOrElse(0)
+        val updatedBoughtSold = updatedBought.copy(holdings = state.holdings.
+          updated(security, num - numSold),cash = state.cash + numSold * todayPrice)
+
+        updatedBoughtSold
+      })
+//      for {
+      // (security, num) <- state.holdings
+      // todayPrice = datePrice(security, state.date)
+      // priceDelta = todayPrice - datePrice(security, state.date - 1)
+      // numBought = if (rules(security).buyThreshold > priceDelta) rules(security).buyCountDailyMax.min(state.cash / todayPrice) else 0
+      // updatedState = state.copy(cash = (state.cash - numBought * todayPrice), holdings = state.holdings.updated(security, numBought + num))
+//        numSold = if (rules(security).sellThreshold < priceDelta) rules(security).sellCountDailyMax.min(num) else 0 updatedState = state.copy(cash = (state.cash + numSold * todayPrice), holdings = state.holdings.updated(security, num - numSold))
+//      }
+
+
+      }
+      def runRules(init:State, stockData: StockData, rules: Map[String,Rule], startDate: Int, endDate: Int): Seq[State] =  {
+        if (startDate > endDate) throw new Exception("stateDate > endDate")
+        (startDate until endDate).foldLeft[Seq[State]](IndexedSeq(init))((states: Seq[State], date: Int) => {
+//          println(eval(states.last, rules).holdings)
+          states :+ eval(states.last, rules)
+        })
       }
 
 
-    }
+
+
+
+
   }
   final case class Rule(security:String, sellThreshold:Double, buyThreshold:Double, buyCountDailyMax:Int, sellCountDailyMax:Int)
 
